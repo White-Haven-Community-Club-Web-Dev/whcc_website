3706eda69a6d00d552612df9743403351cad225d
Legus-Yeung <legus.yeung@gmail.com>
2024-11-10 10:04:51 -0500

    changes

--- WHCFC_Frontend/dist/whcfc-frontend/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-LE22XJTQ.mjs";import"./chunk-PKRCQZBG.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- WHCFC_Frontend/src/app/pages/about-us/about-us.component.ts
+++ WHCFC_Frontend/src/app/pages/about-us/about-us.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { AboutUsSectionComponent } from '../../components/about-us-section/about-us-section.component';
-import { MissionComponent } from '../../components/mission/mission.component';
-import { VisionComponent } from '../../components/vision/vision.component';
-import { GoalComponent } from '../../components/goal/goal.component';
-import { OurBoardComponent } from '../../components/our-board/our-board.component';
-import { WhereWePlayComponent } from '../../components/where-we-play/where-we-play.component';
-import { TestimonialComponent } from '../../components/testimonial/testimonial.component';
+import { MissionComponent } from '../../components/about-us-section/mission/mission.component';
+import { VisionComponent } from '../../components/about-us-section/vision/vision.component';
+import { GoalComponent } from '../../components/about-us-section/goal/goal.component';
+import { OurBoardComponent } from '../../components/about-us-section/our-board/our-board.component';
+import { WhereWePlayComponent } from '../../components/about-us-section/where-we-play/where-we-play.component';
+import { TestimonialComponent } from '../../components/about-us-section/testimonial/testimonial.component';
 
 @Component({
   selector: 'app-about-us',
