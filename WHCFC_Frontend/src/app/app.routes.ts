4d890ece15abae9d70859780c51410388fde8793
Nishant Sharma <nishantsharma2633@gmail.com>
2025-03-15 17:45:16 -0400

    Adds Youth-Program header link and page

--- WHCFC_Frontend/src/app/app.routes.ts
+++ WHCFC_Frontend/src/app/app.routes.ts
@@ -5,18 +5,19 @@ import { NgModule } from '@angular/core';
 import { FaqComponent } from './pages/faq/faq.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { YouthProgramComponent } from './pages/youth-program/youth-program.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'volunteer', component: VolunteerComponent },
-    { path: 'faq', component: FaqComponent },
-    { path: 'contact-us', component: ContactComponent },
-    { path: 'about-us', component: AboutUsComponent }
+  { path: '', component: HomeComponent },
+  { path: 'volunteer', component: VolunteerComponent },
+  { path: 'faq', component: FaqComponent },
+  { path: 'contact-us', component: ContactComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'youth-program', component: YouthProgramComponent },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-
-  export class AppRoutingModule { }
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
