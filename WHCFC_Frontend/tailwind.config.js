cc8382adec61dd14fdad33a704507e087ace16a0
Legus-Yeung <legus.yeung@gmail.com>
2025-03-16 22:34:53 -0400

    tailwind config fixes, minor text adjustment and styling, deleting dist folder

--- WHCFC_Frontend/tailwind.config.js
+++ WHCFC_Frontend/tailwind.config.js
@@ -14,11 +14,6 @@ module.exports = {
         title: ["Holtwood One SC", "serif"],
       },
     },
-    screens: {
-      sm: { max: "767px" }, 
-      md: { min: "768px", max: "1279px" }, 
-      lg: { min: "1280px" }, 
-    },
   },
   plugins: [],
 };
