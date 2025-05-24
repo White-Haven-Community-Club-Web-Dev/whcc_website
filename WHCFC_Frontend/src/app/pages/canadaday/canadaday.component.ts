a85280c381c02cd283e85480542a9abd7346e13d
IKEDA HARUMI <harumimell@gmail.com>
2025-05-24 18:45:22 -0400

    add canada day tournamet page

---
+++ WHCFC_Frontend/src/app/pages/canadaday/canadaday.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-canadaday',
+  standalone: true,
+  imports: [],
+  templateUrl: './canadaday.component.html',
+  styleUrl: './canadaday.component.css'
+})
+export class CanadadayComponent {
+
+}
