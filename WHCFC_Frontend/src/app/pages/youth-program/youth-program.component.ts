95296dd32a9e16bda8dc03f998f33e299bb5c585
IKEDA HARUMI <harumimell@gmail.com>
2025-07-13 17:06:30 -0400

    changed the content for winter events

--- WHCFC_Frontend/src/app/pages/youth-program/youth-program.component.ts
+++ WHCFC_Frontend/src/app/pages/youth-program/youth-program.component.ts
@@ -4,7 +4,7 @@ import { Meta, Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-youth-program',
   templateUrl: './youth-program.component.html',
-  styleUrls: ['./youth-program.component.css']
+  styleUrls: ['./youth-program.component.css'],
 })
 export class YouthProgramComponent implements OnInit, OnDestroy {
   days: string = '00';
@@ -19,9 +19,21 @@ export class YouthProgramComponent implements OnInit, OnDestroy {
     this.startCountdown();
     this.title.setTitle('Youth Program | White Haven Community Football Club');
     this.meta.addTags([
-      { name: 'description', content: "Welcome to the White Haven Community Football Club official website. We are an established not-for-profit football club located in, White Haven Park 63 Invergordon Ave Scarborough, ON, that offers free football experiences, team play and tournaments, community and networking, and free education workshop, Learn more about White Haven Community Football Club's youth program ,Stay active this winter! Join our Subsidized Winter Futsal Program in Scarborough for skill-building, fun, and fitness. Open to all levels" },
-      { name: 'keywords', content: 'football, soccer,futsal, community, club, sports, Scarborough soccer, Scarborough football club, Scarborough soccer club, Scarborough White Haven soccer, Scarborough White Haven football, not-for-profit club, Scarborough youth soccer, Scarborough adult soccer, Scarborough kids soccer, local football club, GTA football, local football club, youth program, company, team, mission, vision, goal' },
-      { name: 'author', content: 'White Haven Community Football Club - Building a Stronger Soccer Community' }
+      {
+        name: 'description',
+        content:
+          "Welcome to the White Haven Community Football Club official website. We are an established not-for-profit football club located in, White Haven Park 63 Invergordon Ave Scarborough, ON, that offers free football experiences, team play and tournaments, community and networking, and free education workshop, Learn more about White Haven Community Football Club's youth program ,Stay active this winter! Join our Subsidized Winter Futsal Program in Scarborough for skill-building, fun, and fitness. Open to all levels",
+      },
+      {
+        name: 'keywords',
+        content:
+          'football, soccer,futsal, community, club, sports, Scarborough soccer, Scarborough football club, Scarborough soccer club, Scarborough White Haven soccer, Scarborough White Haven football, not-for-profit club, Scarborough youth soccer, Scarborough adult soccer, Scarborough kids soccer, local football club, GTA football, local football club, youth program, company, team, mission, vision, goal',
+      },
+      {
+        name: 'author',
+        content:
+          'White Haven Community Football Club - Building a Stronger Soccer Community',
+      },
     ]);
   }
 
@@ -32,7 +44,7 @@ export class YouthProgramComponent implements OnInit, OnDestroy {
   }
 
   startCountdown() {
-    let dest = new Date("March 19, 2025 00:00:00").getTime();
+    let dest = new Date('August 30, 2025 00:00:00').getTime();
 
     this.countdownInterval = setInterval(() => {
       let now = new Date().getTime();
