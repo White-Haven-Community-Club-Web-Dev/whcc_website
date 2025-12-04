ea8b890b030444e7d9542354efbaac1c52fb3106
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2025-12-04 18:33:32 -0500

    Add success to response json and status 400 for invalid token

--- WHCFC_Backend/routes/captcha.js
+++ WHCFC_Backend/routes/captcha.js
@@ -5,34 +5,33 @@ const router = express.Router();
 router.post("/validate", async (req, res) => {
   const { response } = req.body;
 
+  if (!response)
+    return res.status(400).json({ success: false, message: "reCaptcha was not completed" });
+
   try {
-    if (response) {
-      const params = new URLSearchParams();
-      params.append("secret", process.env.CAPTCHA_SECRET);
-      params.append("response", response);
+    const params = new URLSearchParams();
+    params.append("secret", process.env.CAPTCHA_SECRET);
+    params.append("response", response);
 
-      const apiRes = await fetch("https://www.google.com/recaptcha/api/siteverify", {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        },
-        method: "POST",
-        body: params
-      });
+    const apiRes = await fetch("https://www.google.com/recaptcha/api/siteverify", {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      method: "POST",
+      body: params
+    });
 
-      if (!apiRes.ok)
-        throw new Error(`Response status: ${apiRes.status}`);
+    if (!apiRes.ok)
+      throw new Error(`Response status: ${apiRes.status}`);
 
-      const data = await apiRes.json();
+    const data = await apiRes.json();
 
-      return res.status(200).json({ success: data.success })
-    }
+    return res.status(200).json({ success: data.success });
   }
   catch (error) {
     console.log(error);
-    return res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({ success: false, message: "Internal Server Error" });
   }
-
-  res.status(200).json({ success: false });
 });
 
 export default router;
