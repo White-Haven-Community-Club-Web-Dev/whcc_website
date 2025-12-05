a47c511c7ccc21a32cf393bbf3eecff377ef0c1a
Sophie <hdsophie@outlook.com>
2025-12-05 18:35:35 -0500

    fix: restore correct code after merge - fix deploy issues

--- WHCFC_Frontend/src/app/app.routes.ts
+++ WHCFC_Frontend/src/app/app.routes.ts
@@ -14,6 +14,8 @@ import { EducationalSessionsComponent } from './pages/educational-sessions/educa
 import { YouthProgramComponent } from './pages/youth-program/youth-program.component';
 import { CanadadayComponent } from './pages/canadaday/canadaday.component';
 import { WhatWeDoComponent } from './pages/what-we-do/what-we-do.component';
+import { EventListTestComponent } from './components/event-list-test/event-list-test.component';
+import { ArticleListTestComponent } from './components/article-list-test/article-list-test.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -29,7 +31,9 @@ export const routes: Routes = [
   { path: 'educational-sessions', component: EducationalSessionsComponent },
   { path: 'youth-program', component: YouthProgramComponent },
   { path: 'canadaday', component: CanadadayComponent },
-  { path: 'what-we-do', component: WhatWeDoComponent }
+  { path: 'what-we-do', component: WhatWeDoComponent },
+  { path: 'events-test', component: EventListTestComponent }, // CMS 测试 - 活动
+  { path: 'articles-test', component: ArticleListTestComponent }, // CMS 测试 - 文章
 ];
 
 @NgModule({
