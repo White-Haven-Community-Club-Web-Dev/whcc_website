cdf3107e92efb4105f289931c889dbe0d75b8029
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2025-12-04 19:59:12 -0500

    Captcha production site key

--- WHCFC_Frontend/src/environments/environment.ts
+++ WHCFC_Frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   //   apiUrl in this file need to be changed to the actual deployment URL
   apiUrl: 'https://whcc-website.onrender.com',
-  siteKey: "",
+  siteKey: "6LewpSEsAAAAAAJJqUb-CQq20SmdZbzDdJUG3jmu",
 };
