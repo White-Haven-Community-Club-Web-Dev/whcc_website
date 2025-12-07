ef32250c19b2a5ddbd9dea1802cc518d70e81eaf
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2025-12-07 16:16:50 -0500

    Add DB manager; Modify code that uses DB

--- WHCFC_Backend/routes/agenda.js
+++ WHCFC_Backend/routes/agenda.js
@@ -1,14 +1,16 @@
 import express from "express";
-import db from "../db/db.js";
+import DBManager from "../db/db-manager.js";
 
 const router = express.Router();
 
 router.route("/all-agendas").get(async (req, res) => {
-  var sql =
+  const sql =
     "SELECT * FROM agenda WHERE MONTH(date) >= MONTH(CURRENT_DATE()) ORDER BY date ASC, time ASC";
 
   try {
-    var result = await db.query(sql);
+    const pool = await DBManager.getPool();
+
+    var result = await pool.query(sql);
     res.json(result[0]).status(200);
   } catch (error) {
     console.log(error);
@@ -16,20 +18,26 @@ router.route("/all-agendas").get(async (req, res) => {
 });
 
 router.route("/add-agenda").post(async (req, res) => {
-  var sql =
+  const { title, date, time, category, description, location, team1, team2 } = req.body;
+
+  const sql =
     "INSERT INTO agenda (title, date, time, category, description, location, team1, team2) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
   var values = [
-    req.body.title,
-    req.body.date,
-    req.body.time,
-    req.body.category,
-    req.body.description,
-    req.body.location,
-    req.body.team1,
-    req.body.team2,
+    title,
+    date,
+    time,
+    category,
+    description,
+    location,
+    team1,
+    team2,
   ];
+
   try {
-    await db.execute(sql, values);
+    const pool = await DBManager.getPool();
+
+    await pool.execute(sql, values);
     res.json("Agenda added successfully").status(200);
   } catch (error) {
     console.log(error);
