c2f834af2cc0c02393af8b5fd246b8de3015aed2
neolivaresgonzalez <ne.olivaresgonzalez@gmail.com>
2025-12-08 00:12:49 -0500

    Added countdown with isPlatformBrowser to avoid SSR conflict

--- WHCFC_Frontend/src/app/components/what-we-do-section/what-we-do-section.component.ts
+++ WHCFC_Frontend/src/app/components/what-we-do-section/what-we-do-section.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import countdown from 'countdown';
 
 @Component({
   selector: 'app-what-we-do-section',
@@ -7,5 +9,49 @@ import { Component } from '@angular/core';
   templateUrl: './what-we-do-section.component.html',
   styleUrl: './what-we-do-section.component.css'
 })
+export class WhatWeDoSectionComponent implements OnInit, OnDestroy {
+  days_remaining_to_worldcup = 0;
+  hours_remaining_to_worldcup = 0;
+  minutes_remaining_to_worldcup = 0;
 
-export class WhatWeDoSectionComponent { }
+  private timerId: any;
+  private targetDate = new Date('2026-06-11T00:00:00');
+
+  constructor(
+    private cdr: ChangeDetectorRef,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  private updateCountdownOnce() {
+    const timespan = countdown(
+      null,
+      this.targetDate,
+      countdown.DAYS | countdown.HOURS | countdown.MINUTES
+    ) as countdown.Timespan;
+
+    this.days_remaining_to_worldcup = timespan.days ?? 0;
+    this.hours_remaining_to_worldcup = timespan.hours ?? 0;
+    this.minutes_remaining_to_worldcup = timespan.minutes ?? 0;
+  }
+
+  private startBrowserTimer() {
+    this.timerId = setInterval(() => {
+      this.updateCountdownOnce();
+      this.cdr.detectChanges();
+    }, 1000);
+  }
+
+  ngOnInit() { 
+    this.updateCountdownOnce();
+
+    if (isPlatformBrowser(this.platformId)) {
+      this.startBrowserTimer();
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
+}
