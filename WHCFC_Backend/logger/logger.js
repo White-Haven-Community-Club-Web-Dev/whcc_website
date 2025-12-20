c38667274a37c7193fe55593f6336deb0ab9b0d0
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2025-12-20 14:30:46 -0500

    Fix: Output to both console and file instead of one

--- WHCFC_Backend/logger/logger.js
+++ WHCFC_Backend/logger/logger.js
@@ -9,19 +9,25 @@ const fileRotateTransport = new winston.transports.DailyRotateFile({
   maxFiles: process.env.WINSTON_MAX_FILES || 15
 });
 
-const consoleTransport = new winston.transports.Console();
+const consoleTransport = new winston.transports.Console({
+  format: combine(
+    colorize({ all: true })
+  )
+});
 
 const logger = winston.createLogger({
-  colorize: process.env.NODE_ENV === "development" ? colorize() : null,
-  level: process.env.WINSTON_LOG_LEVEL || "info",
+  level: process.env.WINSTON_LOG_LEVEL || "info", // Lowest level to log (e.g. "info":2, it will log "error":0, "warn":1, and "info":2)
   format: combine(
     timestamp({
       format: "YYYY-MM-DD hh:mm:ss A" // 2022-01-25 03:23:10 PM
     }),
-    printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
+    printf(
+      info => `[${info.timestamp}] [${info.level.toUpperCase()}]: ${info.message}`
+    )
   ),
   transports: [
-    process.env.NODE_ENV === "development" ? consoleTransport : fileRotateTransport
+    consoleTransport,
+    fileRotateTransport
   ]
 });
 
