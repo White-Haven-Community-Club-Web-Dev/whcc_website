c563815e5686875032ad37271745b10b3799ee72
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2025-12-20 13:50:06 -0500

    Fix maxFiles

--- WHCFC_Backend/logger/logger.js
+++ WHCFC_Backend/logger/logger.js
@@ -3,6 +3,14 @@ import "winston-daily-rotate-file"
 
 const { combine, timestamp, printf, colorize } = winston.format;
 
+const fileRotateTransport = new winston.transports.DailyRotateFile({
+  filename: "./logs/%DATE%.log",
+  datePattern: "YYYY-MM-DD",
+  maxFiles: process.env.WINSTON_MAX_FILES || 15
+});
+
+const consoleTransport = new winston.transports.Console();
+
 const logger = winston.createLogger({
   colorize: process.env.NODE_ENV === "development" ? colorize() : null,
   level: process.env.WINSTON_LOG_LEVEL || "info",
@@ -13,13 +21,7 @@ const logger = winston.createLogger({
     printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
   ),
   transports: [
-    process.env.NODE_ENV === "development" ?
-      new winston.transports.Console() :
-      new winston.transports.DailyRotateFile({
-        filename: "./logs/%DATE%.log",
-        datePattern: "YYYY-MM-DD",
-        maxFiles: process.env.WINSTON_MAX_DAY || "15d" // Days before automatical delettion
-      })
+    process.env.NODE_ENV === "development" ? consoleTransport : fileRotateTransport
   ]
 });
 
