61093f5a871416562c3c24cea8fc23e17a2c2981
Sophie <hdsophie@outlook.com>
2025-12-30 16:51:10 -0500

    config: update backend API URL to new Render deployment

--- WHCFC_Frontend/src/environments/environment.ts
+++ WHCFC_Frontend/src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  //   apiUrl in this file need to be changed to the actual deployment URL
-  apiUrl: 'https://whcc-website.onrender.com',
+  apiUrl: 'https://whcc-website-backend.onrender.com',
   siteKey: "6LewpSEsAAAAAAJJqUb-CQq20SmdZbzDdJUG3jmu",
 };
