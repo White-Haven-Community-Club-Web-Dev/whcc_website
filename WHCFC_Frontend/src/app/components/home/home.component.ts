c113e40410358e408556a2076242008f50935bd1
Lexie Choi <lexiechoi852@gmail.com>
2026-02-17 01:31:13 -0500

    fix: update House League age range to U7–U12

--- WHCFC_Frontend/src/app/components/home/home.component.ts
+++ WHCFC_Frontend/src/app/components/home/home.component.ts
@@ -213,7 +213,7 @@ export class HomeComponent {
       tag: 'Community',
     },
     {
-      title: 'WHFC Youth House League (Under 7 to Under 11)',
+      title: 'WHFC Youth House League (Ages 7–12)',
       image: 'House League Card Image.png',
       date: 'May – July 2026',
       location: 'Scarborough',
