319f8c36b30b82b6c19c7f32fa14dd457a08770b
Amarpreet Singh <amarpreet1997@gmail.com>
2026-03-27 21:00:07 -0400

    Remove white haven park from the website
    Issue: https://github.com/White-Haven-Community-Club-Web-Dev/whcc_website/issues/102
    Signed-off-by: Amarpreet Singh <amarpreet1997@gmail.com>

--- WHCFC_Frontend/src/app/pages/home/home.component.ts
+++ WHCFC_Frontend/src/app/pages/home/home.component.ts
@@ -109,7 +109,7 @@ export class HomeComponent implements OnInit {
     this.meta.updateTag({
       name: 'description',
       content:
-        "Welcome to the White Haven Community Football Club official website. We are an established not-for-profit football club located in, White Haven Park 63 Invergordon Ave Scarborough, ON, that offers free football experiences, team play and tournaments, community and networking, and free education workshop, Learn more about White Haven Community Football Club's mission, vision, goal and board members",
+        "Welcome to the White Haven Community Football Club official website. We are an established not-for-profit football club located in, Scarborough, ON, that offers free football experiences, team play and tournaments, community and networking, and free education workshop, Learn more about White Haven Community Football Club's mission, vision, goal and board members",
     });
     this.meta.updateTag({
       name: 'keywords',
