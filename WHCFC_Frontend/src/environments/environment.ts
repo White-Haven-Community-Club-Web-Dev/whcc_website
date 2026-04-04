e69bb29c29deb86633cb8e936a37abfe532e50fa
Amarpreet Singh <amarpreet1997@gmail.com>
2026-04-04 15:19:45 -0400

    Point the cms base url to render service

--- WHCFC_Frontend/src/environments/environment.ts
+++ WHCFC_Frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://whcc-website-backend.onrender.com',
   siteKey: "6LewpSEsAAAAAAJJqUb-CQq20SmdZbzDdJUG3jmu",
-  cmsBaseUrl: 'http://localhost:1337',
+  cmsBaseUrl: 'https://whcc-cms.onrender.com',
 };
