97d1d823017d3c6095b879557fd2417a2667ce2a
Robert Devasia <robertdevasia@outlook.com>
2026-09-13 13:23:49 -0400

    Fix section ID reference for in-page navigation

--- WHCFC_Frontend/src/app/components/section/section.component.ts
+++ WHCFC_Frontend/src/app/components/section/section.component.ts
@@ -7,7 +7,7 @@ import { StoryblokComponent } from "@storyblok/angular";
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   template: `
-    <section [id]="blok().anchorId || null" [class]="'pt-16 md:pt-20 pb-8 mx-2 lg:mx-auto  '+blok().background" >
+    <section [id]="blok().sectionId || null" [class]="'pt-16 md:pt-20 pb-8 mx-2 lg:mx-auto  '+blok().background" >
       <sb-component class="mx-auto" [sbBlok]="blok().items" />
     </section>
   `,
@@ -23,5 +23,5 @@ interface ISectionContent {
   items: any[];
   background: string;
   // optional anchor target used by in-page-nav-link
-  anchorId?: string;
+  sectionId?: string;
 }
